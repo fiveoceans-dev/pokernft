c47ee9a648608f034278031acac34eca3dcdf13d
five <niip42@gmail.com>
2025-08-06 18:32:36 +0900

    chores

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -6,12 +6,13 @@ import TrendingSection from "../components/TrendingSection";
 import TournamentsTableSection from "../components/TournamentsTableSection";
 import MarketplaceSection from "../components/MarketplaceSection";
 import StayTunedSection from "../components/StayTunedSection";
-import { Footer } from "../components/Footer";
+import MissionStatementSection from "../components/MissionStatementSection";
 
 export default function HomePage() {
   return (
     <main className="flex flex-col items-stretch">
       <HeroSection />
+      <MissionStatementSection />
       <HowItWorksSection />
       <TrendingSection />
       <TournamentsTableSection />
