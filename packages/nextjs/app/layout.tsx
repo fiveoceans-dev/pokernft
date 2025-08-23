bd76c4eb1e47e63f03b2f99976eeef774275e560
fiveoceans-dev <niip42@gmail.com>
2025-08-24 08:10:36 +0900

    feat: allow mobile access

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata, Viewport } from "next";
 import { ScaffoldStarkAppWithProviders } from "~~/components/ScaffoldStarkAppWithProviders";
 import "~~/styles/globals.css";
 import { ThemeProvider } from "~~/components/ThemeProvider";
-import MobileBlocker from "~~/components/MobileBlocker";
 
 export const metadata: Metadata = {
   title: "PokerNFTs",
@@ -23,10 +22,7 @@ const ScaffoldStarkApp = ({ children }: { children: React.ReactNode }) => {
         className="flex flex-col min-h-screen bg-main"
       >
         <ThemeProvider>
-          <MobileBlocker />
-          <ScaffoldStarkAppWithProviders>
-            {children}
-          </ScaffoldStarkAppWithProviders>
+          <ScaffoldStarkAppWithProviders>{children}</ScaffoldStarkAppWithProviders>
         </ThemeProvider>
       </body>
     </html>
